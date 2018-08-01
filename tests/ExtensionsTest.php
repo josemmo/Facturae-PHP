4687e04f5cf3da5e82ff1767e6aa95eee045d395
José Miguel Moreno <josemmo@pm.me>
2018-08-01 21:00:35 +0200

    Actualizada extensión FACeB2B
    - Actualizada extensión FACeB2B para cumplir con la nueva versión del schema (1.1)
    - Actualizada documentación
    - Actualizado test de extensiones

--- tests/ExtensionsTest.php
+++ tests/ExtensionsTest.php
@@ -37,24 +37,40 @@ final class ExtensionsTest extends TestCase {
     ]));
     $fac->addItem("Línea de producto", 100, 1, Facturae::TAX_IVA, 10);
 
-    // Añadimos los destinatarios de FACeB2B
+    // Obtener la extensión de FACeB2B y establecemos la entidad pública
     $b2b = $fac->getExtension('Fb2b');
+    $b2b->setPublicOrganismCode('E00003301');
+    $b2b->setContractReference('333000');
+
+    // Añadimos los remitentes (vendedores) de FACeB2B
+    $b2b->addCentre(new FacturaeCentre([
+      "code" => "ES12345678Z0002",
+      "name" => "Unidad DIRe Vendedora 0002",
+      "role" => FacturaeCentre::ROLE_B2B_SELLER
+    ]), false);
+    $b2b->addCentre(new FacturaeCentre([
+      "code" => "ES12345678Z0003",
+      "name" => "Unidad DIRe Fiscal 0003",
+      "role" => FacturaeCentre::ROLE_B2B_FISCAL
+    ]), false);
+
+    // Añadimos los destinatarios (compradores) de FACeB2B
     $b2b->setReceiver(new FacturaeCentre([
       "code" => "51558103JES0001",
       "name" => "Centro administrativo receptor"
     ]));
     $b2b->addCentre(new FacturaeCentre([
-      "code" => "51558103JES0002",
+      "code" => "ESB123456740002",
       "name" => "Unidad DIRe Compradora 0002",
       "role" => FacturaeCentre::ROLE_B2B_BUYER
     ]));
     $b2b->addCentre(new FacturaeCentre([
-      "code" => "51558103JES0003",
+      "code" => "ESB123456740003",
       "name" => "Unidad DIRe Fiscal 0003",
       "role" => FacturaeCentre::ROLE_B2B_FISCAL
     ]));
     $b2b->addCentre(new FacturaeCentre([
-      "code" => "51558103JES0004",
+      "code" => "ESB123456740004",
       "role" => FacturaeCentre::ROLE_B2B_COLLECTOR
     ]));
 
