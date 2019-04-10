9015ffbd40a70bd2a2024fbb6b8b8e2953c1ccb5
José Miguel Moreno <josemmo@pm.me>
2019-04-10 21:10:43 +0200

    Añadido certificado de WS (#47)
    - Creado certificado para Web Services (FACe y FACeB2B)
    - Actualizados AbstractTest y WebservicesTest
    
    NOTA: certificado obtenido del "Kit de certificados de prueba de FNMT-RCM (03/05/2018)" desde la página web https://ws024.juntadeandalucia.es/ae/adminelec/areatecnica/afirma

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -8,5 +8,6 @@ abstract class AbstractTest extends TestCase {
   const OUTPUT_DIR = __DIR__ . "/output";
   const CERTS_DIR = __DIR__ . "/certs";
   const FACTURAE_CERT_PASS = "12345";
+  const WEBSERVICES_CERT_PASS = "G5cp,fYC9gje";
 
 }
--- tests/WebservicesTest.php
+++ tests/WebservicesTest.php
@@ -10,7 +10,7 @@ final class WebservicesTest extends AbstractTest {
    * Test FACe
    */
   public function testFace() {
-    $face = new FaceClient(self::CERTS_DIR . "/facturae.pfx", null, self::FACTURAE_CERT_PASS);
+    $face = new FaceClient(self::CERTS_DIR . "/webservices.p12", null, self::WEBSERVICES_CERT_PASS);
     $face->setProduction(false);
     $res = $face->getStatus();
 
@@ -23,7 +23,7 @@ final class WebservicesTest extends AbstractTest {
    * Test FACeB2B
    */
   public function testFaceb2b() {
-    $face = new Faceb2bClient(self::CERTS_DIR . "/facturae.pfx", null, self::FACTURAE_CERT_PASS);
+    $face = new Faceb2bClient(self::CERTS_DIR . "/webservices.p12", null, self::WEBSERVICES_CERT_PASS);
     $face->setProduction(false);
     $res = $face->getCodes();
 
