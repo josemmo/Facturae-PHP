657cca79c80a07c5894ce73030b52f5587858ff8
José Miguel Moreno <josemmo@pm.me>
2020-11-21 10:38:21 +0100

    Actualizado certificado webservices
    - Sustituido certificado para webservices de FACe/FACeB2B al caducar el anterior
    
    > NOTA: el nuevo certificado pertenece al kit de pruebas de producción de IZENPE S.A., y es concretamente "representante_sw_act.p12"

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -10,7 +10,7 @@ abstract class AbstractTest extends TestCase {
   const OUTPUT_DIR = __DIR__ . "/output";
   const CERTS_DIR = __DIR__ . "/certs";
   const FACTURAE_CERT_PASS = "12345";
-  const WEBSERVICES_CERT_PASS = "G5cp,fYC9gje";
+  const WEBSERVICES_CERT_PASS = "741258";
   const NOTIFICATIONS_EMAIL = "josemmo@pm.me";
   const COOKIES_PATH = self::OUTPUT_DIR . "/cookies.txt";
 
